e7cdc1ba0ab6d58f8644b0f0c623dd4dcdb6124f
bretweinraub <bret@ckuru.com>
2011-05-02 12:25:56 +0000

    Works for all Med ETLs

--- lib/aura/etl_helper.rb
+++ lib/aura/etl_helper.rb
@@ -7,19 +7,38 @@ module Aura
     attr_accessor :targetDB
     attr_accessor :sourceRD,:targetRD
 
+    #
+    # when the original primary key was 'id'; we map it to 'original_id'
+    #
+    def original_primary_key
+      if sourceRD.naturalKey == "id"
+        "original_id"
+      else
+        sourceRD.naturalKey
+      end
+    end
+
     #takes a set of keys and rewrites them for a replicated table
     def rewrite_attributes(h={})
-      row, record_class = only_these_parameters(h,
-                                    [:row,{:instance_that_inherits_from => ActiveRecord::Base,:required => true}],
-                                    [:record_class,{:klass_that_inherits_from => ActiveRecord::Base,:required => true}]
-                                    )
+      row, original_primary_key, record_class \
+        = only_these_parameters(h,
+                                [:row,{:instance_that_inherits_from => ActiveRecord::Base,:required => true}],
+                                [:original_primary_key,{:instance_that_inherits_from => String,:required => true}],
+                                [:record_class,{:klass_that_inherits_from => ActiveRecord::Base,:required => true}]
+                                )
 
 
       new = Hash.new
       row.attributes.keys.each do |k|
         new_key = k.downcase
         if new_key == record_class.primary_key
-          new_key = "original_#{new_key}"
+          if new_key == original_primary_key
+            ckebug 0, "mapping #{new_key} from #{record_class.primary_key}"
+            new_key = "original_#{new_key}"
+          else
+            ckebug 1, "skipping attribute #{new_key}; assuming this is the system generated key"
+            next
+          end
         end
         new[new_key] = row.attributes[k]
       end
